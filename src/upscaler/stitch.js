2c0ea0316f9833524b95d96a9fa7c7cef18acf41
Lemuel <is101.lemuel@gmail.com>
2022-06-02 07:10:36 +0800

    fix: parse frame rate properly

--- src/upscaler/stitch.js
+++ src/upscaler/stitch.js
@@ -2,10 +2,12 @@ const ffmpeg = require("fluent-ffmpeg");
 
 function stitchFrames(input, output, metadata, setProgress) {
   return new Promise((resolve, reject) => {
-    const framerate = metadata.framerate * 2;
+    const [a, b] = metadata.r_frame_rate.split("/");
+    const framerate = parseInt(a) / parseInt(b);
+    const newFramerate = framerate * 2;
 
     ffmpeg(input)
-      .inputOptions([`-r ${framerate}`])
+      .inputOptions([`-r ${newFramerate}`])
       .output(output)
       .outputOptions(["-c:v libx264", "-pix_fmt yuv420p"])
       .on("progress", ({ percent }) => {
