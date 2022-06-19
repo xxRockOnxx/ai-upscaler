b5e402b929af6ec4a67754ed3ffec68f6ca2f9a9
Lemuel <is101.lemuel@gmail.com>
2022-06-19 18:01:42 +0800

    refactor: remove unnecessary conditional

--- packages/api/src/server-worker.js
+++ packages/api/src/server-worker.js
@@ -4,16 +4,12 @@ const createJobLogger = require('./jobs');
 const createQueue = require('./queue');
 const jobsWorker = require('./worker/jobs');
 
-if (require.main === module) {
-  const redisURL = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-  const redisDB = createClient({ url: redisURL });
-  const queueDB = createQueue(redisDB);
-  const jobsDB = createJobLogger(redisDB);
-  const upscaleQueue = new Bull('upscale', redisURL);
+const redisURL = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const redisDB = createClient({ url: redisURL });
+const queueDB = createQueue(redisDB);
+const jobsDB = createJobLogger(redisDB);
+const upscaleQueue = new Bull('upscale', redisURL);
 
-  redisDB.connect();
+redisDB.connect();
 
-  jobsWorker(queueDB, jobsDB, upscaleQueue);
-} else {
-  //
-}
+jobsWorker(queueDB, jobsDB, upscaleQueue);
