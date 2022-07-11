7d3ec51585523b7d04d64ac199c5e79ede7a23c2
Lemuel <is101.lemuel@gmail.com>
2022-07-12 04:22:24 +0800

    fix(core): minio should delete recursively if `id` is folder

--- packages/core/src/storage/minio.ts
+++ packages/core/src/storage/minio.ts
@@ -13,7 +13,40 @@ export function createStorage(minio: Client, bucket: string, prefix = ''): Stora
     },
 
     async delete(id) {
-      await minio.removeObject(bucket, path.join(prefix, id));
+      const files: string[] = await new Promise((resolve, reject) => {
+        const returnvalue = [];
+        minio
+          .listObjects(bucket, path.join(prefix, id), true)
+          .on('data', (data) => {
+            returnvalue.push(data.name);
+          })
+          .on('end', () => resolve(returnvalue))
+          .on('error', reject);
+      });
+
+      if (files.length === 0) {
+        return;
+      }
+
+      const file = files[0];
+
+      // Remove trailing slash
+      const fullPrefix = path.join(prefix, id).replace(/\/$/, '');
+
+      // If next character is '/' that means a folder is requested to be removed.
+      // Example:
+      // files[0] = 'uploads/123/raw/frame.png'
+      // 'uploads' + 1 = '/'
+      // 'upl' + 1 = 'o'
+      const isFolder = file[file.indexOf(fullPrefix) + fullPrefix.length] === '/';
+
+      // Prevent unwanted delete.
+      // Exact match means file delete (folder/subfolder/file.ext)
+      if (file !== fullPrefix && !isFolder) {
+        return;
+      }
+
+      await minio.removeObjects(bucket, files);
     },
   };
 }
